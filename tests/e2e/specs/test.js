68a2abcde03cd33d2d15fc55e7bd1d3de3664b02
Victor <admin@victorciurana.com>
2021-06-10 10:49:07 +0200

    A simple test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,19 @@
-// https://docs.cypress.io/api/introduction/api.html
+describe('Home page to post blog', () => {
 
-describe('My First Test', () => {
-  it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+  it('NAvigate the simple app', () => {
+    // Home displays H1
+    cy.visit('http://localhost:8080/')
+    cy.contains('h1', 'Victor Ciurana')
+
+    // Click on a post to read
+    cy.contains('READ').click()
+
+    // The post has a H2 and contains some text
+    cy.get('h2').first().contains(/^[\w\ ]+/)
+
+    // Goes to the about page
+    cy.contains('About').click().get('#app').contains('About')
+
   })
+  
 })
